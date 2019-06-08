03c0ebcfebb2bf282a98144fa40ade1ca02dd76d
Tomi Mavrin <tomi.mavrin@gmail.com>
2019-06-08 14:18:14 +0200

    settings scene

---
+++ Unity Project/space_shooter/Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] public int networkPort = 4455;
+    [SerializeField] public string networkAddress = "localhost";
+
+    [SerializeField] Text ipAddress;
+
+    NetworkManager networkM;
+        
+    public void Start() {
+        networkM = GetComponent<NetworkManager>();
+        networkM.networkPort = networkPort;
+        networkM.networkAddress = networkAddress;
+
+    }
+
+    public void OnCustomizationButton() {
+        SceneManager.LoadScene("ShipCustomMenu");
+    }
+
+
+    public void OnHostButton() {
+        networkM.StartHost();
+    }
+
+    public void OnJoinGameButton() {
+        if (ipAddress.text == "") {
+            networkM.networkAddress = "localhost";
+        }
+        else {
+            networkM.networkAddress = ipAddress.text;
+        }
+
+
+        Debug.Log(ipAddress.text);
+        Debug.Log(networkM.networkAddress);
+        networkM.StartClient();
+    }
+}
---
+++ Unity Project/space_shooter/Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SettingsManager : MonoBehaviour
+{
+    [SerializeField] public Sprite[] shipSprites;
+    [SerializeField] public Image shipImage;
+
+    private int shipI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        shipI = PlayerPrefs.GetInt("ShipShape");
+        shipImage.sprite = shipSprites[shipI];
+    }
+
+    public void OnNextButton() {
+        shipImage.sprite = shipSprites[Mathf.Abs((shipI++) % 3)];
+    }
+
+    public void OnPrevButton() {
+        shipImage.sprite = shipSprites[Mathf.Abs((shipI--) % 3)];
+    }
+
+    public void OnExitButton() {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
